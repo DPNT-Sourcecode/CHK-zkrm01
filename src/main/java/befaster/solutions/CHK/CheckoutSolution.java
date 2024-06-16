f76c85b28b51b20028ac047dd9efdc7f65780f58
sbx_user1051 <sbx_user1051@169.254.87.165>
2024-06-16 11:55:15 +0000

    Sun Jun 16 11:55:15 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,6 +2,7 @@ package befaster.solutions.CHK;
 import java.util.HashMap;
 import java.util.Map;
 import befaster.runner.SolutionNotImplementedException;
+import com.sun.source.tree.ReturnTree;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
@@ -18,27 +19,37 @@ public class CheckoutSolution {
             itemCounts.put(sku, itemCounts.getOrDefault(sku, 0) + 1);
         }
         int total = 0;
-        total += calculatePrice(itemCounts, 'A', 50, 3,130);
-        total += calculatePrice(itemCounts, 'B', 30, 2,45);
-        total += calculatePrice(itemCounts, 'C', 20, 0,0);
-        total += calculatePrice(itemCounts, 'D', 15, 0,0);
+        total += calculatePrice(itemCounts, 'A', 50, new int[]{5,3}, new int[]{200, 130});
+        total += calculatePrice(itemCounts, 'B', 30, new int[]{5,3}, new int[]{200, 130});
+        total += calculatePrice(itemCounts, 'C', 20, new int[]{5,3}, new int[]{200, 130});
+        total += calculatePrice(itemCounts, 'D', 15, new int[]{5,3}, new int[]{200, 130});
 
         return total;
     }
 
 
-    private int calculatePrice(Map<Character, Integer> itemCounts, char item, int unitPrice, int specialQuantity, int specialPrice)
-    {
+    private int calculatePrice(Map<Character, Integer> itemCounts, char item, int unitPrice, int[] specialQuantity, int[] specialPrice) {
         int count = itemCounts.getOrDefault(item, 0);
-        if (specialQuantity > 0 && count >= specialQuantity)
-        {
-            int specialCount = count/ specialQuantity;
-            int remainder = count % specialQuantity;
-            return specialCount * specialPrice + remainder * unitPrice;
-        }else
-        {
-            return count * unitPrice;
-
+        for (int i = 0; i < specialQuantity.length; i++) {
+            int specialCount = count / specialQuantity[i];
+            total += specialCount * specialPrice[i];
         }
+        total += count * unitPrice;
+        return total;
+
     }
+
+    private int calculatePriceWithFreeItem(Map<Character, Integer> itemCounts, char item, int unitPrice, int offerQuantity,char freeItem ){
+        int count = itemCounts.getOrDefault(item, 0);
+        int freeItemCount = itemCounts.getOrDefault(freeItem, 0);
+
+        int freeItemsToAdd = count / freeItemCount;
+        freeItemCount = Math.max(0,freeItemCount - freeItemsToAdd);
+
+        itemCounts.put(freeItem,freeItemCount);
+
+        return count * unitPrice;
+
+    }
+
 }
