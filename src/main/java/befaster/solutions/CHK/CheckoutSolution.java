2ec3419603df4411756beff2d5d63ce995ee994c
sbx_user1051 <sbx_user1051@169.254.87.165>
2024-06-16 11:38:41 +0000

    Sun Jun 16 11:38:41 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -12,11 +12,33 @@ public class CheckoutSolution {
         }
         Map<Character, Integer> itemCounts = new HashMap<>();
         for (char sku : skus.toCharArray()){
-            if (sku != 'A')
-
+            if (sku != 'A' && sku != 'B' && sku != 'C' && sku != 'D'){
+                return -1; //invalid input
+            }
+            itemCounts.put(sku, itemCounts.getOrDefault(sku, 0) + 1);
         }
+        int total = 0;
+        total += calculatePrice(itemCounts, 'A', 50, 3,130);
+        total += calculatePrice(itemCounts, 'B', 50, 3,130);
 
 
     }
+
+
+    private int calculatePrice(Map<Character, Integer> itemCounts, char item, int unitPrice, int specialQuantity, int specialPrice)
+    {
+        int count = itemCounts.getOrDefault(item, 0);
+        if (specialQuantity > 0 && count >= specialQuantity)
+        {
+            int specialCount = count/ specialQuantity;
+            int remainder = count % specialQuantity;
+            return specialCount * spefcialPrice + remainder * unitPrice;
+        }else
+        {
+            return count * unitPrice;
+
+        }
+    }
 }
+
 
